b4e094e8fdf7a34095ae71d9b338dd23368581a5
NIVETHAK21 <nivethakanagaraj2105@gmail.com>
2026-08-29 11:12:04 +0530

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/srisaibuilders/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
